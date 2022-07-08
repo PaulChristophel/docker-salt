463415643434239d5d0602ae15f52edb8828bbd1
Paul C. Martin <pmartin@gatech.edu>
2022-07-08 11:08:24 -0400

    Force pyzmq==21.0.2 on python 3.10

--- build.sh
+++ build.sh
@@ -5,23 +5,36 @@ if [ -z "$BUILD" ]; then
 fi
 
 docker pull python:3.9-alpine
-docker build -f Dockerfile-3.9-alpine . \
-             -t oitacr.azurecr.io/pmartin47/salt:3.9-latest-alpine \
-             -t oitacr.azurecr.io/pmartin47/salt:3.9-$BUILD-alpine \
-             -t oitacr.azurecr.io/pmartin47/salt:3.9-latest \
-             -t oitacr.azurecr.io/pmartin47/salt:3.9-$BUILD
-
+if [[ ${BUILD} != *"b"* ]]; then
+    docker build -f Dockerfile-3.9-alpine --build-arg BUILD=$BUILD . \
+                 -t oitacr.azurecr.io/pmartin47/salt:3.9-latest-alpine \
+                 -t oitacr.azurecr.io/pmartin47/salt:3.9-$BUILD-alpine \
+                 -t oitacr.azurecr.io/pmartin47/salt:3.9-latest \
+                 -t oitacr.azurecr.io/pmartin47/salt:3.9-$BUILD
+else
+    docker build -f Dockerfile-3.9-alpine --build-arg BUILD=$BUILD . \
+                 -t oitacr.azurecr.io/pmartin47/salt:3.9-$BUILD-alpine \
+                 -t oitacr.azurecr.io/pmartin47/salt:3.9-$BUILD
+fi
 
 docker pull python:3.10-alpine
-docker build -f Dockerfile-3.10-alpine . \
-             -t oitacr.azurecr.io/pmartin47/salt:3.10-latest-alpine \
-             -t oitacr.azurecr.io/pmartin47/salt:3.10-$BUILD-alpine \
-             -t oitacr.azurecr.io/pmartin47/salt:latest-alpine \
-             -t oitacr.azurecr.io/pmartin47/salt:$BUILD-alpine \
-             -t oitacr.azurecr.io/pmartin47/salt:3.10-latest \
-             -t oitacr.azurecr.io/pmartin47/salt:3.10-$BUILD \
-             -t oitacr.azurecr.io/pmartin47/salt:latest \
-             -t oitacr.azurecr.io/pmartin47/salt:$BUILD
+if [[ ${BUILD} != *"b"* ]]; then
+	docker build -f Dockerfile-3.10-alpine --build-arg BUILD=$BUILD . \
+        	     -t oitacr.azurecr.io/pmartin47/salt:3.10-latest-alpine \
+	             -t oitacr.azurecr.io/pmartin47/salt:3.10-$BUILD-alpine \
+        	     -t oitacr.azurecr.io/pmartin47/salt:latest-alpine \
+	             -t oitacr.azurecr.io/pmartin47/salt:$BUILD-alpine \
+        	     -t oitacr.azurecr.io/pmartin47/salt:3.10-latest \
+	             -t oitacr.azurecr.io/pmartin47/salt:3.10-$BUILD \
+        	     -t oitacr.azurecr.io/pmartin47/salt:latest \
+	             -t oitacr.azurecr.io/pmartin47/salt:$BUILD
+else
+	docker build -f Dockerfile-3.10-alpine --build-arg BUILD=$BUILD . \
+                     -t oitacr.azurecr.io/pmartin47/salt:3.10-$BUILD-alpine \
+                     -t oitacr.azurecr.io/pmartin47/salt:$BUILD-alpine \
+                     -t oitacr.azurecr.io/pmartin47/salt:3.10-$BUILD \
+                     -t oitacr.azurecr.io/pmartin47/salt:$BUILD
+fi
 
 #docker pull python:3.11-rc-alpine
 #docker build -f Dockerfile-3.11-alpine . \
@@ -33,6 +46,8 @@ docker build -f Dockerfile-3.10-alpine . \
 
 az acr login -n oitacr
 docker images | awk "/$BUILD/ { print \$1\":\"\$2 }" | xargs -I {} docker push {}
-docker images | awk "/pmartin47\/salt/&&/latest/ { print \$1\":\"\$2 }" | xargs -I {} docker push {}
-docker images | awk "/pmartin47\/salt/&&/3.9-latest/ { print \$1\":\"\$2 }" | xargs -I {} docker push {}
-docker images | awk "/pmartin47\/salt/&&/3.10-latest/ { print \$1\":\"\$2 }" | xargs -I {} docker push {}
+if [[ ${BUILD} != *"b"* ]]; then
+	docker images | awk "/pmartin47\/salt/&&/latest/ { print \$1\":\"\$2 }" | xargs -I {} docker push {}
+	docker images | awk "/pmartin47\/salt/&&/3.9-latest/ { print \$1\":\"\$2 }" | xargs -I {} docker push {}
+	docker images | awk "/pmartin47\/salt/&&/3.10-latest/ { print \$1\":\"\$2 }" | xargs -I {} docker push {}
+fi
