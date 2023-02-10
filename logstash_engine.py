dbb21b15353f930fa7746885f90a035decbec442
Paul C. Martin <pmartin@gatech.edu>
2023-02-10 08:51:24 -0500

    Rename reserved variables

--- logstash_engine.py
+++ logstash_engine.py
@@ -59,6 +59,50 @@ def event_bus_context(opts):
     return event_bus
 
 
+def validate(data):
+    """
+    Validate the event data and ensure there are no
+    conflicts with skip_list
+    """
+    skip_list = {
+        "args",
+        "asctime",
+        "created",
+        "exc_info",
+        "exc_text",
+        "filename",
+        "funcName",
+        "id",
+        "levelname",
+        "levelno",
+        "lineno",
+        "module",
+        "msecs",
+        "msecs",
+        "message",
+        "msg",
+        "name",
+        "pathname",
+        "process",
+        "processName",
+        "relativeCreated",
+        "thread",
+        "threadName",
+        "extra",
+        "auth_token",
+        "password",
+        "stack_info",
+    }
+
+    matches = skip_list.intersection(data)
+    for mat in matches:
+        if mat in ('password', 'auth_token'):
+            _ = data.pop(mat)
+            continue
+        data[mat + "_"] = data.pop(mat)
+    return data
+
+
 def start(
     host,
     port=5959,
@@ -75,6 +119,7 @@ def start(
 
     if isinstance(host, list):
         from random import choice  # pylint: disable=import-outside-toplevel
+
         host = choice(host)
     if proto == "tcp":
         logstash_handler = logstash.TCPLogstashHandler
@@ -89,5 +134,5 @@ def start(
         log.debug("Logstash engine started")
         for event in event_bus.iter_events(full=True, auto_reconnect=True):
             if event and "data" in event and "tag" in event:
-                tag, data = event["tag"], event["data"]
+                tag, data = event["tag"], validate(event["data"])
                 logstash_logger.info(tag, extra=data)
